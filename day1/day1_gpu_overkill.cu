89b54d9a070420fb77bb5775441aeb423a181fd8
Nick Shively <njshively1354@gmail.com>
2021-12-03 02:05:20 -0800

    CUDA hijinks

--- day1/day1_gpu_overkill.cu
+++ day1/day1_gpu_overkill.cu
@@ -26,6 +26,15 @@ vectorGreaterThan(const int *A, bool* B, int size) {
     
 }
 
+__global__ void
+vector3dSum(const int *A, int* B, int size) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i <= 1 || i >= size) {
+        return;
+    }
+    B[i-2] = A[i] + A[i-1] + A[i-2];
+//    printf("test val b: i[%d] B[%d]\n", i, B[i-2]); 
+}
 
 int main() {
     std::vector<int> inData;
@@ -33,17 +42,15 @@ int main() {
     File.open("input.txt");
     while(!File.eof())
     {
-        // Now you may be saying 'Nick, why arent you just incrementing a counter every time a value increases right here, rather than do extra work?
-        // Because fuck you, lets use this gpu for something it has no business doing
         int p = 0;
         File >> p;
         if (File.eof()) break;
         inData.push_back(p);
-        std::cout << p << std::endl;
     }
     int numElements = inData.size();
     size_t sizeInput = numElements * sizeof(int);
     size_t sizeOutput = (numElements-1) * sizeof(bool);
+    size_t sizeOutput2 = 2*(numElements-2) * sizeof(int);
     int *host_input = (int*)malloc(sizeInput);
     bool *host_output = (bool*)malloc(sizeOutput);
     host_input = inData.data();   
@@ -71,6 +78,7 @@ int main() {
 
     int threadsPerBlock=256;
     int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
+
     vectorGreaterThan<<<blocksPerGrid, threadsPerBlock>>>(d_input, d_output, sizeInput);
     err = cudaGetLastError();
 
@@ -90,6 +98,33 @@ int main() {
     for (int i = 0; i < sizeOutput; i++) {
         if (host_output[i]) total++;
     }
-    std::cout << "total: " << total << std::endl;
+    std::cout << "part A total: " << total << std::endl;
 
+    int* host_output_pt2 = (int*)malloc(sizeOutput2);
+    int* d_output_2 = NULL;
+    err = cudaMalloc((void**)&d_output_2, sizeOutput2);
+    if (err != cudaSuccess)
+    {
+        std::cout << "error pt2 malloc " << cudaGetErrorString(err) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    vector3dSum<<<blocksPerGrid, threadsPerBlock>>>(d_input, d_output_2, numElements);
+    err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        std::cout << "error in pt2 kernl: " << cudaGetErrorString(err) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    err = cudaMemcpy(host_output_pt2, d_output_2, sizeOutput2, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess)
+    {
+        std::cout << "error copy back to host pt2: " << cudaGetErrorString(err) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    int total2 = 0;
+    for (int i = 1; i < numElements-2; i++)
+    {
+        if (host_output_pt2[i] > host_output_pt2[i-1]) total2++;
+    }
+    std::cout << "part B total: " << total2 << std::endl;
 }
